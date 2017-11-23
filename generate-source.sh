8f5d5c13f492989f086407caa53c41b2378d83e2
Daniel Jasiński <daniel.jasinski@codilime.com>
2017-11-23 08:18:26 +0100

    Clean generated API classes during Maven clean phase.

--- generate-source.sh
+++ generate-source.sh
@@ -4,7 +4,7 @@
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
 
-TARGET=src/net/juniper/contrail/api/types
+TARGET=$1
 
 mkdir -p ${TARGET}
 ../generateds/generateDS.py -f -o ${TARGET} -g java-api ../controller/src/schema/vnc_cfg.xsd
