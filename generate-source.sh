5c5545873cb6ea36213a79cf13ec0ec19e93adbc
Wojciech Urbański <wojciech.urbanski@codilime.com>
2018-03-21 14:11:04 +0100

    Adjust generate-source.sh to location in sandbox
    Change-Id: I12df423dabca109a81018ce68cfc0048e2091861
    Depends-On: I88fe456a4bcd3415bf43937bfd5a042ea46a9f0e
    Partial-Bug: 1751339

--- generate-source.sh
+++ generate-source.sh
@@ -3,8 +3,9 @@
 #
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TARGET=$1
+SBTOP=${2:-${DIR}/..}
 
 mkdir -p ${TARGET}
-../generateds/generateDS.py -f -o ${TARGET} -g java-api ../contrail-api-client/schema/vnc_cfg.xsd
+$SBTOP/tools/generateds/generateDS.py -f -o ${TARGET} -g java-api $SBTOP/src/contrail-api-client/schema/vnc_cfg.xsd
