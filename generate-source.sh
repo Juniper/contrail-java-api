bebd4984e011e408f60cf13270b3a8834a72faf8
Daniel Jasiński <daniel.jasinski@codilime.com>
2018-03-22 08:55:04 +0100

    Move layout information to Makefile
    Change-Id: Iaf74ad96346960d355b1d3ce3224539f9c8b7ffe
    Partial-Bug: 1751339

--- generate-source.sh
+++ generate-source.sh
@@ -3,9 +3,8 @@
 #
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 TARGET=$1
-SBTOP=${2:-${DIR}/..}
 
 mkdir -p ${TARGET}
-$SBTOP/tools/generateds/generateDS.py -f -o ${TARGET} -g java-api $SBTOP/src/contrail-api-client/schema/vnc_cfg.xsd
+../generateds/generateDS.py -f -o ${TARGET} -g java-api ../contrail-api-client/schema/vnc_cfg.xsd
